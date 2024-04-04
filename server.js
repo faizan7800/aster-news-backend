7e7d3ed3414d6c8272e7731a3bd144fe1c20ffad
Faizan <faiza.mahn122@gmail.com>
2024-04-04 16:44:06 +0500

    changed database url

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ dotenv.config({
   path: "./config.env",
 }); // read all the variables from config.env file and put them in nodejs environment
 const app = require("./app");
-const DB = "mongodb://localhost/newsAppDB";
+const DB = "mongodb+srv://faizan:faizan123@cluster0.j4fztss.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 // ! Connecting our App with hosted datatbase on Atlas Cloud
 
 mongoose
